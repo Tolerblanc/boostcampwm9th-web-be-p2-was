8abb5da7812e7476f585a3da6c7d5a5f57fb4358
Hyunjun KIM <hihj070914@icloud.com>
2024-09-24 14:47:25 +0900

    feat: static index response
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>
    Co-authored-by: seoko97 <seoko97@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,55 @@
 import net from "node:net";
 import { logger } from "./util/logger";
+import { extname, join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import { existsSync, readFileSync } from "node:fs";
+
+function getDirname(importMetaUrl: string) {
+  return dirname(fileURLToPath(importMetaUrl));
+}
+
+const __dirname = getDirname(import.meta.url);
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     logger.info(data.toString());
-    socket.end();
+    const [requestHeader, body] = data.toString().split("\r\n\r\n");
+    const [firstLine, ...options] = requestHeader.split("\r\n");
+    const [method, uri, protocol] = firstLine.split(" ");
+
+    if (method === "GET") {
+      try {
+        const ext = extname(uri);
+
+        if (ext) {
+          const rootDir = join(__dirname, "../");
+          const filePath = join(rootDir, "/views", uri);
+
+          if (!existsSync(filePath)) {
+            const error = new Error("Not Found");
+            error.status = 404;
+            throw error;
+          }
+
+          const data = readFileSync(filePath);
+
+          socket.write("HTTP/1.1 200 OK\r\n");
+          socket.write("Content-Type: text/html\r\n");
+          socket.write("\r\n");
+          socket.write(data);
+        } else {
+          // TODO: 라우트요청
+        }
+      } catch (e) {
+        socket.write(`HTTP/1.1 ${e.status} ${e.message}`);
+        socket.write("Content-Type: text/plain\r\n");
+        socket.write("\r\n");
+        socket.write(e.message);
+        logger.error(`${method} ${uri} ${e.message}`);
+      } finally {
+        socket.end();
+      }
+    }
   });
 });
 
