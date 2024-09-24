faf21c2a5618a38cb9f58322b829c5af7b82e64b
Hyunjun KIM <hihj070914@icloud.com>
2024-09-24 13:29:42 +0900

    feat: default server & logger
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>
    Co-authored-by: seoko97 <seoko97@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("!");
+import net from "node:net";
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    logger.info(data.toString());
+    socket.end();
+  });
+});
+
+server.listen(3000, () => {
+  logger.info("3000번 포트에서 서버 대기 중");
+});
