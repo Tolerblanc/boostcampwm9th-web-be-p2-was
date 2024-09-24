500c9a084c45c5fa4a77522b5b00bc04e82f5cf7
Hyunjun KIM <hihj070914@icloud.com>
2024-09-24 16:28:11 +0900

    feat: create endpoint - 임시 DB (json) 생성 - 유저 생성 및 검사 로직 작성 (임시) - /create API 구현
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>
    Co-authored-by: seoko97 <seoko97@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import net from "node:net";
 import { logger } from "./util/logger";
 import { extname, join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
-import { existsSync, readFileSync } from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
+
+type TUser = {
+  userId: string;
+  name: string;
+  email: string;
+  password: string;
+};
 
 function getDirname(importMetaUrl: string) {
   return dirname(fileURLToPath(importMetaUrl));
@@ -28,6 +35,7 @@ const CONTENT_TYPE = {
   [EXT_NAME.png]: "image/png",
   [EXT_NAME.jpg]: "image/jpeg",
   [EXT_NAME.js]: "text/javascript",
+  json: "application/json",
 } as const;
 
 function serveStaticFile(socket: net.Socket, uri: string) {
@@ -52,17 +60,31 @@ function serveStaticFile(socket: net.Socket, uri: string) {
   socket.write(data);
 }
 
+function parseQueryParameters(str: string) {
+  const queries = str.split("&").map((data) => data.split("="));
+
+  return queries.reduce<Record<string, string>>((acc, cur) => {
+    const [key, value] = cur;
+
+    acc[key] = value;
+
+    return acc;
+  }, {});
+}
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const [requestHeader, body] = data.toString().split("\r\n\r\n");
     const [firstLine, ...options] = requestHeader.split("\r\n");
     const [method, uri, protocol] = firstLine.split(" ");
 
+    const [endpoint, queryString] = uri.split("?");
+
     logger.info(`${protocol} ${method} ${uri}`);
 
     if (method === "GET") {
       try {
-        const ext = extname(uri).slice(1);
+        const ext = extname(endpoint).slice(1);
 
         if (ext) {
           if (!EXT_NAME[ext]) {
@@ -75,7 +97,43 @@ const server = net.createServer((socket) => {
           // *미들웨어: 정적 파일 서빙
           serveStaticFile(socket, uri);
         } else {
-          // TODO: 라우트요청
+          if (endpoint === "/create") {
+            const query = parseQueryParameters(queryString);
+
+            const { userId, password, name, email } = query;
+
+            const rootDir = join(__dirname, "../");
+            const dbPath = join(rootDir, "db", "user.json");
+
+            const users = JSON.parse(
+              readFileSync(dbPath).toString()
+            ) as TUser[];
+
+            const existUser = users.find((user) => {
+              return (
+                user.name === name ||
+                user.userId === userId ||
+                user.email === email
+              );
+            });
+
+            if (existUser) {
+              const error = new Error("Aleady Exist");
+              error.status = 409;
+              throw error;
+            }
+
+            const newUser = { userId, password, email, name };
+
+            users.push(newUser);
+
+            writeFileSync(dbPath, JSON.stringify(users));
+
+            socket.write("HTTP/1.1 200 OK\r\n");
+            socket.write(`Content-Type: ${CONTENT_TYPE.json}\r\n`);
+            socket.write("\r\n");
+            socket.write(JSON.stringify(newUser));
+          }
         }
       } catch (e) {
         socket.write(`HTTP/1.1 ${e.status} ${e.message}`);
