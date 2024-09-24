1f2be39b934b126cb49ae75b8db20545f83c84ea
Hyunjun KIM <hihj070914@icloud.com>
2024-09-24 13:47:41 +0900

    feat: detach logger
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>
    Co-authored-by: seoko97 <seoko97@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,5 @@
 import net from "node:net";
-import { createLogger, format, transports } from "winston";
-
-const { printf, combine, timestamp, label, colorize } = format;
-
-const logFormat = printf(({ level, message, label, timestamp }) => {
-  return `[${level}] ${timestamp} | ${label}: ${message}`; // [level] timestamp label: message
-});
-
-const logger = createLogger({
-  level: "info",
-  format: combine(
-    colorize(),
-    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    label({ label: "Web-BE-P1 WAS" }),
-    logFormat
-  ),
-  transports: [new transports.Console()],
-});
+import { logger } from "./util/logger";
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
---
+++ src/util/logger.ts
@@ -0,0 +1,18 @@
+import { createLogger, format, transports } from "winston";
+
+const { printf, combine, timestamp, label, colorize } = format;
+
+const logFormat = printf(({ level, message, label, timestamp }) => {
+  return `[${level}] ${timestamp} | ${label}: ${message}`; // [level] timestamp label: message
+});
+
+export const logger = createLogger({
+  level: "info",
+  format: combine(
+    colorize(),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    label({ label: "Web-BE-P1 WAS" }),
+    logFormat
+  ),
+  transports: [new transports.Console()],
+});
