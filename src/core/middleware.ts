39f6269d152b47be2a12a1b64dd53752f0a55602
Hyunjun KIM <hihj070914@icloud.com>
2024-09-26 16:43:55 +0900

    feat: 임시 middleware 클래스 작성 - TODO: 라우터 핸들링은 어떻게 할지 고민
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>

---
+++ src/core/middleware.ts
@@ -0,0 +1,31 @@
+import { Request } from "@/core/request";
+import { Response } from "@/core/response";
+
+type MiddlewareHandler = (
+  req: Request,
+  res: Response,
+  next: () => void
+) => void | Promise<void>;
+
+class Middleware {
+  private handlers: MiddlewareHandler[];
+  constructor() {
+    this.handlers = [];
+  }
+
+  use(handler: MiddlewareHandler) {
+    this.handlers.push(handler);
+  }
+
+  handle(req: Request, res: Response): Promise<void> {
+    const chainedHandler = async (index: number) => {
+      if (index === this.handlers.length) {
+        return;
+      }
+      await this.handlers[index](req, res, () => chainedHandler(index + 1));
+    };
+    return chainedHandler(0);
+  }
+}
+
+export { MiddlewareHandler, Middleware };
