40f743e995be6899e59589c99e640e39785a13d2
Hyunjun KIM <hihj070914@icloud.com>
2024-09-26 16:46:08 +0900

    feat: serveStatic 시그니처 변경 - Request, Response 객체를 사용하도록 변경함 - sendReponse 삭제 (Response 클래스가 해당 역할 대체)
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>

--- src/util/serveStatic.ts
+++ src/util/serveStatic.ts
@@ -1,21 +1,21 @@
-import net from "net";
 import { join, extname } from "node:path";
 import { readFile, access, constants } from "node:fs/promises";
 
 import { getDirname } from "./getDirname";
 import { NotFoundError } from "@/util/httpError";
-import { sendResponse } from "@/util/sendResponse";
 import { CONTENT_TYPE, ExtName } from "@/constants/contentType.enum";
+import { Request } from "@/core/request";
+import { Response } from "@/core/response";
 
 const __dirname = getDirname(import.meta.url);
 
-async function serveStaticFile(socket: net.Socket, uri: string) {
-  const ext = extname(uri).slice(1).toLowerCase() as ExtName;
+async function serveStaticFile(req: Request, res: Response) {
+  const ext = extname(req.uri).slice(1).toLowerCase() as ExtName;
 
   const dir = ext === "html" ? "views" : "static";
 
   const rootDir = join(__dirname, "../../");
-  const filePath = join(rootDir, `/${dir}`, uri);
+  const filePath = join(rootDir, `/${dir}`, req.uri);
 
   try {
     await access(filePath, constants.R_OK);
@@ -25,12 +25,9 @@ async function serveStaticFile(socket: net.Socket, uri: string) {
 
   const data = await readFile(filePath);
 
-  sendResponse(socket, {
-    status: 200,
-    message: "OK",
-    contentType: CONTENT_TYPE[ext],
-    data: data.toString(),
-  });
+  res.contentType = CONTENT_TYPE[ext];
+  res.data = data.toString();
+  res.send();
 }
 
 export { serveStaticFile };
