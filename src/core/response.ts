1288868afa6c4cc3df2b57be8f72b644778b877a
Hyunjun KIM <hihj070914@icloud.com>
2024-09-26 16:44:54 +0900

    feat: 임시 Response 클래스 작성
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>

---
+++ src/core/response.ts
@@ -0,0 +1,30 @@
+import { CONTENT_TYPE } from "@/constants/contentType.enum";
+import net from "node:net";
+
+class Response {
+  //TODO: 빌더 패턴으로 변경하기
+  private socket: net.Socket;
+  accessor status: number;
+  accessor message: string;
+  accessor contentType: string;
+  accessor data: string | null;
+
+  constructor(socket: net.Socket) {
+    this.socket = socket;
+    this.status = 200;
+    this.message = "OK";
+    this.contentType = CONTENT_TYPE.txt;
+    this.data = null;
+  }
+
+  send() {
+    this.socket.write(`HTTP/1.1 ${this.status} ${this.message}\r\n`);
+    this.socket.write(`Content-Type: ${this.contentType}\r\n`);
+    this.socket.write("\r\n");
+    if (this.data) this.socket.write(this.data);
+
+    this.socket.end();
+  }
+}
+
+export { Response };
