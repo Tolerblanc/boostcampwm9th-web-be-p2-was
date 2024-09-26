4b1599b3997ac9a194453320c54354f0e9427fc6
Hyunjun KIM <hihj070914@icloud.com>
2024-09-26 17:48:58 +0900

    fix: 라우터 핸들 함수가 uri를 참조하고 있는 오류 수정 - 엔드포인트로 바꿈
    Co-authored-by: dudejrs <dudejrs@users.noreply.github.com>

--- src/core/router.ts
+++ src/core/router.ts
@@ -1,17 +1,10 @@
 import { MiddlewareHandler } from "@/core/middleware";
 import { Request } from "@/core/request";
 import { Response } from "@/core/response";
-import { NotFoundError } from "@/util/httpError";
 
 class Router {
-  // 라우팅 어떻게 하죠?? ㅜㅜ
-  // 메서드 기준 / URI 기준
-  // 메서드 기준이면 key:value 중 value 가 너무 비대해진다? 동의합니다
-  // URI 기준이면 빠르게 탐색 되면서 value가 최대 5개?
-  // 근데 만약 /user/:id 같은 동적 라우팅이 있으면 어떻게 하죠??
-  // 일단 URI 기준으로 구현해보고, 나중에 생각해봅시다
-
   private routeTable: Map<string, Map<string, MiddlewareHandler>>; // URI, <Method, Handler>
+
   constructor() {
     this.routeTable = new Map();
   }
@@ -43,11 +36,11 @@ class Router {
     //TODO: DELETE 메서드 라우팅
   }
 
-  handle: MiddlewareHandler = async (req, res) => {
+  handle = async (req: Request, res: Response) => {
     const method = req.method.toUpperCase();
-    const uri = req.uri;
+    const endpoint = req.endpoint;
 
-    const handler = this.routeTable.get(uri)?.get(method);
+    const handler = this.routeTable.get(endpoint)?.get(method);
     if (handler) {
       await handler(req, res, () => {});
     }
