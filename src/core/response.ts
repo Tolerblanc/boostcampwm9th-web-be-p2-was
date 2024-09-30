54bf608af214fe2eb8008b70b9da5be24e2bf277
Hyunjun KIM <hihj070914@icloud.com>
2024-09-30 20:50:06 +0900

    refactor: Response 빌더 패턴 적용

--- src/core/response.ts
+++ src/core/response.ts
@@ -2,26 +2,43 @@ import { CONTENT_TYPE } from "@/constants/contentType.enum";
 import net from "node:net";
 
 class Response {
-  //TODO: 빌더 패턴으로 변경하기
-  private socket: net.Socket;
-  accessor status: number;
-  accessor message: string;
-  accessor contentType: string;
-  accessor data: string | null;
+  private _status: number;
+  private _message: string;
+  private _contentType: string;
+  private _data: string;
 
-  constructor(socket: net.Socket) {
-    this.socket = socket;
-    this.status = 200;
-    this.message = "OK";
-    this.contentType = CONTENT_TYPE.txt;
-    this.data = null;
+  constructor(private _socket: net.Socket) {
+    this._status = 200;
+    this._message = "OK";
+    this._contentType = CONTENT_TYPE.txt;
+    this._data = "";
+  }
+
+  status(status: number) {
+    this._status = status;
+    return this;
+  }
+
+  message(message: string) {
+    this._message = message;
+    return this;
+  }
+
+  contentType(contentType: string) {
+    this._contentType = contentType;
+    return this;
+  }
+
+  data(data: string) {
+    this._data = data;
+    return this;
   }
 
   send() {
-    this.socket.write(`HTTP/1.1 ${this.status} ${this.message}\r\n`);
-    this.socket.write(`Content-Type: ${this.contentType}\r\n`);
-    this.socket.write("\r\n");
-    if (this.data) this.socket.write(this.data);
+    this._socket.write(`HTTP/1.1 ${this._status} ${this._message}\r\n`);
+    this._socket.write(`Content-Type: ${this._contentType}\r\n`);
+    this._socket.write("\r\n");
+    if (this._data) this._socket.write(this._data);
   }
 }
 
