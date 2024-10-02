861dec5dbfb0fd5584f512f2880217e27df17ff0
Hyunjun KIM <hihj070914@icloud.com>
2024-10-02 18:25:23 +0900

    feat: 버퍼링 로직 추가

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,18 +1,20 @@
 import net from "node:net";
-import { extname } from "node:path";
 
-import {
-  HttpError,
-  NotFoundError,
-  UnsupportedMediaTypeError,
-} from "@/util/httpError";
+import { HttpError, NotFoundError } from "@/util/httpError";
 import { logger } from "@/util/logger";
-import { CONTENT_TYPE, EXT_NAME, ExtName } from "@/constants/contentType.enum";
-import { serveStaticFile } from "@/util/serveStatic";
+import { CONTENT_TYPE } from "@/constants/contentType.enum";
 import { Middleware, MiddlewareHandler } from "@/core/middleware";
 import { Request } from "@/core/request";
 import { Response } from "@/core/response";
-import { Router } from "./router";
+import { Router } from "@/core/router";
+import { handleStaticFileRoute } from "@/core/builtin/staticFile.middleware";
+
+// TODO: 클래스로 분리
+type ConnectionBuffer = {
+  data: Buffer;
+  size: number;
+  maxSize: number;
+};
 
 class WasApplication {
   private middleware: Middleware;
@@ -27,52 +29,101 @@ class WasApplication {
   }
 
   init() {
-    this.middleware.use(async (request, response) => {
-      //TODO: static serving 미들웨어 분리, 존재하는 파일을 모두 라우터에 등록하는 로직 고려해보기
-      const ext = extname(request.endpoint).slice(1).toLowerCase() as ExtName;
-      if (ext) {
-        if (!EXT_NAME.includes(ext)) {
-          throw new UnsupportedMediaTypeError();
-        }
-
-        await serveStaticFile(request, response);
-      }
-    });
+    //TODO: 존재하는 파일을 모두 라우터에 등록하는 로직 고려해보기
+    this.middleware.use(handleStaticFileRoute);
 
     this.server = net.createServer((socket) => {
-      socket.on("data", async (data) => {
-        //TODO: 버퍼 처리 로직 추가
-        const request = new Request(data.toString());
-        const response = new Response(socket);
-        logger.info(request.toString());
+      const connectionBuffer: ConnectionBuffer = {
+        data: Buffer.alloc(0),
+        size: 0,
+        maxSize: 1024 * 1024 * 10, // TODO: 환경변수로 관리
+      };
 
-        try {
-          await this.middleware.handle(request, response);
-          await this.router.handle(request, response);
-          if (!response.isSent) {
-            //TODO: 미들웨어로 예외 처리 로직 분리
-            throw new NotFoundError();
-          }
-        } catch (e) {
-          const {
-            status = 500,
-            message = "Internal Server Error",
-            stack,
-          } = e as HttpError;
+      socket.on("timeout", () => {
+        logger.error(
+          `Connection Timeout ${socket.remoteAddress}:${socket.remotePort}`
+        );
+        socket.end();
+      });
 
-          response
-            .status(status)
-            .message(message)
-            .contentType(CONTENT_TYPE["txt"])
-            .data(message)
-            .send();
+      socket.on("data", async (chunk) => {
+        if (connectionBuffer.size + chunk.length > connectionBuffer.maxSize) {
+          logger.error(
+            `Connection Buffer Overflow ${socket.remoteAddress}:${socket.remotePort}`
+          );
+          socket.end();
+          return;
+        }
 
-          logger.error(`${request.method} ${request.uri}\n${stack}`);
-        } finally {
-          if (socket.writable) socket.end();
+        connectionBuffer.data = Buffer.concat([connectionBuffer.data, chunk]);
+        connectionBuffer.size += chunk.length;
+
+        if (this.isCompleteHttpRequest(connectionBuffer)) {
+          this.processHttpRequest(socket, connectionBuffer);
+          connectionBuffer.data = Buffer.alloc(0);
+          connectionBuffer.size = 0;
+        }
+      });
+
+      socket.on("end", () => {
+        if (connectionBuffer.size > 0) {
+          logger.error(
+            `Connection End ${socket.remoteAddress}:${socket.remotePort}`
+          );
         }
       });
     });
+  }
+
+  private isCompleteHttpRequest(connectionBuffer: ConnectionBuffer) {
+    const str = connectionBuffer.data.toString();
+    const headerEnd = str.indexOf("\r\n\r\n");
+
+    if (headerEnd === -1) return false;
+
+    const contentLengthMatch = str.match(/Content-Length: (\d+)/i);
+    if (contentLengthMatch) {
+      const contentLength = parseInt(contentLengthMatch[1], 10);
+      return connectionBuffer.data.length >= headerEnd + 4 + contentLength;
+    }
+
+    // * Content-Length 헤더가 없는 경우, 헤더의 끝을 요청의 끝으로 간주
+    return true;
+  }
+
+  private async processHttpRequest(
+    socket: net.Socket,
+    connectionBuffer: ConnectionBuffer
+  ) {
+    const request = new Request(connectionBuffer.data.toString());
+    const response = new Response(socket);
+
+    logger.info(request.toString());
+
+    try {
+      await this.middleware.handle(request, response);
+      await this.router.handle(request, response);
+      if (!response.isSent) {
+        throw new NotFoundError();
+      }
+    } catch (e) {
+      const {
+        status = 500,
+        message = "Internal Server Error",
+        stack,
+      } = e as HttpError;
+
+      response
+        .status(status)
+        .message(message)
+        .contentType(CONTENT_TYPE["txt"])
+        .data(message)
+        .send();
+
+      logger.error(`${request.method} ${request.uri}\n${stack}`);
+    } finally {
+      if (socket.writable) socket.end();
+    }
   }
 
   use(handler: Router) {
@@ -81,6 +132,10 @@ class WasApplication {
 
   get(path: string, handler: MiddlewareHandler) {
     this.router.get(path, handler);
+  }
+
+  post(path: string, handler: MiddlewareHandler) {
+    this.router.post(path, handler);
   }
 
   listen(port: number) {
