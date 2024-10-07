143db843dcc878fa20e1b971c664bc77d83c7e51
Hyunjun KIM <hihj070914@icloud.com>
2024-10-07 17:05:41 +0900

    feat: 임시 로그인 핸들러 작성

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import { WasApplication } from "./core/app";
 import { createUser } from "./users/createUser";
 import { Request } from "@/core/request";
 import { Response } from "@/core/response";
+import { login } from "./users/login";
 
 async function createHandler(request: Request, response: Response) {
   //TODO: 컨트롤러로 분리
   const newUser = await createUser(request.body);
 
   response.created().contentType("json").data(newUser).send();
+}
+
+async function loginHandler(request: Request, response: Response) {
+  const username = await login(request.body);
+  response.cookie("username", username).redirect("/").send();
 }
 
 function redirectToIndex(request: Request, response: Response) {
@@ -19,6 +25,7 @@ function bootstrap() {
 
   app.get("/", redirectToIndex);
   app.post("/create", createHandler);
+  app.post("/login", loginHandler);
 
   app.listen(3000);
 }
