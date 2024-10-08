5ec50041cd7d5d08409fc1d6c377f0656238fdff
Hyunjun KIM <hihj070914@icloud.com>
2024-10-08 16:27:37 +0900

    refactor: 기존 구현한 회원가입/로그인 로직 이동

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post } from "@/core/util/decorators";
 import { Request } from "@/core/http/request";
 import { Response } from "@/core/http/response";
+import { createUser } from "@/user/createUser";
+import { login } from "./login";
 
 @Controller("/user")
 class UserController {
@@ -9,9 +11,18 @@ class UserController {
     res.status(200).data({ message: "Users list" }).send();
   }
 
-  @Post("/")
+  @Post("/login")
+  async login(req: Request, res: Response): Promise<void> {
+    //TODO: 서비스 레이어 분리
+    const username = await login(req.body);
+    res.cookie("username", username).redirect("/").send();
+  }
+
+  @Post("/create")
   async createUser(req: Request, res: Response): Promise<void> {
-    res.status(201).data({ message: "User created" }).send();
+    //TODO: 서비스 레이어 분리
+    const newUser = await createUser(req.body);
+    res.created().contentType("json").data(newUser).send();
   }
 
   @Get("/:id/detail")
