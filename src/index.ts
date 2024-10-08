5044a8ddbdb13184b259eaf4886f13829ad5ff5b
Hyunjun KIM <hihj070914@icloud.com>
2024-10-08 16:28:26 +0900

    feat: 사용하지 않는 함수 제거, 인덱스 컨트롤러 등록

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,11 @@
 import { WasApplication } from "./core/app";
-import { createUser } from "./user/createUser";
-import { Request } from "@/core/http/request";
-import { Response } from "@/core/http/response";
-import { login } from "@/user/login";
 import { UserController } from "@/user/user.controller";
-import { handleStaticFileRoute } from "./core/builtin/staticFile.middleware";
-
-async function createHandler(request: Request, response: Response) {
-  //TODO: 컨트롤러로 분리
-  const newUser = await createUser(request.body);
-
-  response.created().contentType("json").data(newUser).send();
-}
-
-async function loginHandler(request: Request, response: Response) {
-  const username = await login(request.body);
-  response.cookie("username", username).redirect("/").send();
-}
-
-function redirectToIndex(request: Request, response: Response) {
-  response.redirect("/index.html").send();
-}
+import { IndexController } from "@/index.controller";
 
 function bootstrap() {
   const app = new WasApplication();
 
-  app.use(handleStaticFileRoute);
+  app.registerControllers(IndexController);
   app.registerControllers(UserController);
 
   app.listen(3000);
