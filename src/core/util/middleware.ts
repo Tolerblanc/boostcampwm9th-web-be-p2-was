8105559946831be995f75f32e577ccf338a34f0e
Hyunjun KIM <hihj070914@icloud.com>
2024-10-08 16:09:13 +0900

    feat: 재설계에 맞도록 미들웨어와 라우터 재작성 - 라우터를 radix 트리구조로 변경하여 효율적인 경로 파싱 - 미들웨어 클래스를 삭제하고, 타입으로만 선언 (함수로 작성하여 체인에 추가할 수 있도록 변경)

--- src/core/util/middleware.ts
+++ src/core/util/middleware.ts
@@ -1,31 +1,12 @@
 import { Request } from "@/core/http/request";
 import { Response } from "@/core/http/response";
 
-type MiddlewareHandler = (
+type NextFunction = () => void | Promise<void>;
+
+type MiddlewareFunction = (
   req: Request,
   res: Response,
-  next: () => void
+  next: NextFunction
 ) => void | Promise<void>;
 
-class Middleware {
-  private handlers: MiddlewareHandler[];
-  constructor() {
-    this.handlers = [];
-  }
-
-  use(handler: MiddlewareHandler) {
-    this.handlers.push(handler);
-  }
-
-  handle(req: Request, res: Response): Promise<void> {
-    const chainedHandler = async (index: number) => {
-      if (index === this.handlers.length) {
-        return;
-      }
-      await this.handlers[index](req, res, () => chainedHandler(index + 1));
-    };
-    return chainedHandler(0);
-  }
-}
-
-export { MiddlewareHandler, Middleware };
+export type { MiddlewareFunction, NextFunction };
--- src/core/util/router.ts
+++ src/core/util/router.ts
@@ -1,50 +1,104 @@
-import { MiddlewareHandler } from "@/core/util/middleware";
+import { MiddlewareFunction } from "@/core/util/middleware";
 import { Request } from "@/core/http/request";
 import { Response } from "@/core/http/response";
+import { NotFoundError } from "@/core/http/httpError";
+
+type RouteHandler = MiddlewareFunction;
+
+/**
+ * 라우팅 트리의 노드
+ * @class Node
+ * @property {Object} children - 자식 노드들
+ * @property {Node} paramChild - 동적 라우팅 노드
+ * @property {Object} handlers - 메서드별 핸들러 목록
+ * @property {string} paramName - 동적 라우팅 노드의 이름
+ */
+class Node {
+  children: { [key: string]: Node } = {};
+  paramChild: Node | null = null;
+  handlers: { [method: string]: RouteHandler[] } | null = null;
+  paramName: string | null = null;
+}
 
 class Router {
-  private routeTable: Map<string, Map<string, MiddlewareHandler>>; // URI, <Method, Handler>
+  private root: Node = new Node();
 
-  constructor() {
-    this.routeTable = new Map();
-  }
-
-  private addRoute(uri: string, method: string, handler: MiddlewareHandler) {
-    if (!this.routeTable.has(uri)) {
-      this.routeTable.set(uri, new Map());
+  addRoute(method: string, path: string, handlers: RouteHandler[]) {
+    let node = this.root;
+    const parts = path.split("/").filter((part) => part.length > 0);
+    for (const part of parts) {
+      // 동적 라우팅 경로라면
+      // ? 더 깔끔하게 작성할 수 없을까?
+      if (part.startsWith(":")) {
+        if (!node.paramChild) {
+          node.paramChild = new Node();
+          node.paramChild.paramName = part.slice(1);
+        }
+        node = node.paramChild;
+      } else {
+        if (!node.children[part]) {
+          node.children[part] = new Node();
+        }
+        node = node.children[part];
+      }
     }
-    this.routeTable.get(uri)?.set(method, handler);
+    if (!node.handlers) {
+      node.handlers = {};
+    }
+    if (!node.handlers[method]) {
+      node.handlers[method] = [];
+    }
+    node.handlers[method].push(...handlers);
   }
 
-  get(path: string, handler: MiddlewareHandler) {
-    this.addRoute(path, "GET", handler);
+  async handle(req: Request, res: Response) {
+    const parts = req.path.split("/").filter((p) => p);
+    const params: { [key: string]: string } = {};
+
+    const node = this.findNode(this.root, parts, params);
+
+    if (node?.handlers && node.handlers[req.method]) {
+      req.params = params;
+      await this.runMiddleware(req, res, node.handlers[req.method]);
+    } else {
+      throw new NotFoundError();
+    }
   }
 
-  post(path: string, handler: MiddlewareHandler) {
-    this.addRoute(path, "POST", handler);
-  }
-
-  put() {
-    //TODO: PUT 메서드 라우팅
-  }
-
-  patch() {
-    //TODO: PATCH 메서드 라우팅
-  }
-
-  delete() {
-    //TODO: DELETE 메서드 라우팅
-  }
-
-  handle = async (req: Request, res: Response) => {
-    const method = req.method.toUpperCase();
-    const endpoint = req.endpoint;
-
-    const handler = this.routeTable.get(endpoint)?.get(method);
-    if (handler) {
+  private async runMiddleware(
+    req: Request,
+    res: Response,
+    handlers: MiddlewareFunction[]
+  ) {
+    for (const handler of handlers) {
       await handler(req, res, () => {});
+      if (res.isSent) break;
     }
-  };
+  }
+
+  private findNode(
+    node: Node,
+    parts: string[],
+    params: { [key: string]: string }
+  ): Node | null {
+    if (parts.length === 0) {
+      return node;
+    }
+
+    const part = parts[0];
+    const remainingParts = parts.slice(1);
+
+    if (node.children[part]) {
+      return this.findNode(node.children[part], remainingParts, params);
+    }
+
+    if (node.paramChild) {
+      params[node.paramChild.paramName!] = part;
+      return this.findNode(node.paramChild, remainingParts, params);
+    }
+
+    return null;
+  }
 }
 
 export { Router };
