a7808109b5080163c3e9acfde115d3afc64a100c
Hyunjun KIM <hihj070914@icloud.com>
2024-10-08 16:11:18 +0900

    feat: 라우트 데코레이터 작성 - 컨트롤러 클래스를 모아 라우터에 등록할 수 있도록 함 - 각 HTTP 메서드에 대한 메서드 데코레이터 작성

---
+++ src/core/util/decorators.ts
@@ -0,0 +1,111 @@
+import "reflect-metadata";
+import { Middleware } from "@/core/util/middleware";
+
+function Controller(prefix: string = ""): ClassDecorator {
+  return function (constructor: Function) {
+    Reflect.defineMetadata("prefix", prefix, constructor);
+  };
+}
+
+function Get(path: string): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    registerRoute("GET", path, target, propertyKey);
+    return descriptor;
+  };
+}
+
+function Post(path: string): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    registerRoute("POST", path, target, propertyKey);
+    return descriptor;
+  };
+}
+
+function Put(path: string): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    registerRoute("PUT", path, target, propertyKey);
+    return descriptor;
+  };
+}
+
+function Patch(path: string): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    registerRoute("PATCH", path, target, propertyKey);
+    return descriptor;
+  };
+}
+
+function Delete(path: string): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    registerRoute("DELETE", path, target, propertyKey);
+    return descriptor;
+  };
+}
+
+function UseMiddleware(...middlewares: Middleware[]): MethodDecorator {
+  return function (
+    target: any,
+    propertyKey: string | symbol,
+    descriptor: PropertyDescriptor
+  ) {
+    const existingMiddlewares =
+      Reflect.getMetadata("middlewares", target, propertyKey) || [];
+    Reflect.defineMetadata(
+      "middlewares",
+      [...existingMiddlewares, ...middlewares],
+      target,
+      propertyKey
+    );
+    return descriptor;
+  };
+}
+
+interface RouteDefinition {
+  method: string;
+  path: string;
+  handlerName: string | symbol;
+}
+
+function registerRoute(
+  method: string,
+  path: string,
+  target: any,
+  propertyKey: string | symbol
+) {
+  const constructor = target.constructor;
+  const routes: RouteDefinition[] =
+    Reflect.getMetadata("routes", constructor) || [];
+  routes.push({ method, path, handlerName: propertyKey });
+  Reflect.defineMetadata("routes", routes, constructor);
+}
+
+export {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Patch,
+  Delete,
+  UseMiddleware,
+  RouteDefinition,
+};
