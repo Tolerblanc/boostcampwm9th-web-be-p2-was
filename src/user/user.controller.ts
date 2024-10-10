e422f02b6f5045feb9e4a7ff3509c71d519e8195
Hyunjun KIM <hihj070914@icloud.com>
2024-10-10 16:15:32 +0900

    feat: UserController 작성 - 서비스 계층으로 비즈니스 로직 분리 예정 - TODO: 의존성 주입을 생성자 기반으로 교체

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,37 +1,33 @@
 import { Controller, Get, Post } from "@/core/util/decorators";
 import { Request } from "@/core/http/request";
 import { Response } from "@/core/http/response";
-import { createUser } from "@/user/createUser";
-import { login } from "./login";
+import { UserService } from "@/user/user.service";
+import { UserRepository } from "@/user/user.repository";
 
 @Controller("/user")
 class UserController {
-  @Get("/")
+  private readonly userService: UserService;
+  constructor() {
+    //TODO: 생성자 기반 의존성 주입
+    this.userService = new UserService(UserRepository);
+  }
+
+  @Get("/list")
   async getUsers(req: Request, res: Response): Promise<void> {
-    res.status(200).data({ message: "Users list" }).send();
+    const users = await this.userService.getUserList();
+    res.status(200).data(users).send();
   }
 
   @Post("/login")
   async login(req: Request, res: Response): Promise<void> {
-    //TODO: 서비스 레이어 분리
-    const username = await login(req.body);
-    res.cookie("username", username).redirect("/").send();
+    const token = await this.userService.login(req.body);
+    res.redirect("/").data({ token }).send();
   }
 
   @Post("/create")
   async createUser(req: Request, res: Response): Promise<void> {
-    //TODO: 서비스 레이어 분리
-    const newUser = await createUser(req.body);
+    const newUser = await this.userService.create(req.body);
     res.created().contentType("json").data(newUser).send();
-  }
-
-  @Get("/:id/detail")
-  async getUser(req: Request, res: Response) {
-    const userId = req.params.id;
-    res
-      .status(200)
-      .data({ message: `User ${userId} details` })
-      .send();
   }
 }
 
