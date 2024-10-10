cfacc2b6465220e257460b3835d53d8f40e89909
Hyunjun KIM <hihj070914@icloud.com>
2024-10-10 18:21:06 +0900

    chore: jest 설정 변경 - 데코레이터 파싱을 못해서 관련된 의존성 추가

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 export default {
-  transform: {
-    "^.+\\.ts$": "babel-jest", // .ts 파일을 babel-jest로 변환
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
-  testRegex: "(/test/.*|(\\.|/)(test))\\.ts$", // .test.ts 파일에서 테스트 찾기
-  moduleFileExtensions: ["ts", "js"], // Jest가 처리할 확장자 목록
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.json",
+        babelConfig: true,
+      },
+    ],
+  },
 };
