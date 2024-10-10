bfaa65ce6f5a8a160068db7b76743b1e67bff6d1
Hyunjun KIM <hihj070914@icloud.com>
2024-10-10 18:20:33 +0900

    feat: 인증 미들웨어에서 실패시 401을 반환하도록 변경

--- src/core/builtin/auth.middleware.ts
+++ src/core/builtin/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { Request } from "@/core/http/request";
 import { Response } from "@/core/http/response";
 import { NextFunction, MiddlewareFunction } from "@/core/util/middleware";
+import { UnauthorizedError } from "@/core/http/httpError";
 import SessionStore from "@/core/util/sessionStore";
 
 const AuthMiddleware: MiddlewareFunction = async (
@@ -11,8 +12,7 @@ const AuthMiddleware: MiddlewareFunction = async (
   const token = req.headers.authorization;
   const session = SessionStore.get(token);
   if (!session) {
-    res.redirect(`/login.html?redirectUrl=${req.path}`).send();
-    return;
+    throw new UnauthorizedError();
   }
   req.params.userId = session.userId;
   req.params.userName = session.userName;
