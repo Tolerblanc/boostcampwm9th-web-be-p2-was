7a891dd58c030dcc716d722899bda0effb2b1cde
Hyunjun KIM <hihj070914@icloud.com>
2024-10-10 16:16:23 +0900

    feat: 유저 서비스 계층 구현 - 기존 로그인/회원가입 로직 이관 - 모든 유저 리스트를 반환하는 로직 작성

---
+++ src/user/user.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestError, UnauthorizedError } from "@/core/http/httpError";
+import { UserRepository } from "./user.repository";
+import SessionStore from "@/core/util/sessionStore";
+
+class UserService {
+  constructor(private readonly userRepository: typeof UserRepository) {}
+
+  async getUserList() {
+    const userList = await this.userRepository.list();
+    return userList.map((user) => {
+      return {
+        email: user.email,
+        nickname: user.nickname,
+        createdAt: user.createdAt,
+      };
+    });
+  }
+
+  private validateLoginBody(body: Record<string, string>) {
+    const { email, password } = body;
+    if (!email || !password)
+      throw new BadRequestError("Invalid email or password");
+    //TODO: 이메일 형식 검증 필요
+    return { email, password };
+  }
+
+  async login(body: Record<string, string>) {
+    const { email, password } = this.validateLoginBody(body);
+    const user = await this.userRepository.findByEmail(email);
+    const isPasswordCorrect = password === user?.password;
+    if (!user || !isPasswordCorrect)
+      throw new UnauthorizedError("Invalid email or password");
+    const sessionId = SessionStore.create(user.id, user.nickname);
+    return sessionId;
+  }
+
+  async create(body: Record<string, string>) {
+    //TODO: 추가 검증 로직 필요
+    //TODO: 패스워드 암호화 필요
+    const user = await this.userRepository.createUser({
+      email: body.email,
+      password: body.password,
+      nickname: body.nickname,
+    });
+    return {
+      email: user.email,
+      nickname: user.nickname,
+      createdAt: user.createdAt,
+    };
+  }
+}
+
+export { UserService };
