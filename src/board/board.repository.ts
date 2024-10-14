a6a9ab6386a9e2852d93b83f977eca5ada811443
Hyunjun KIM <hihj070914@icloud.com>
2024-10-14 22:13:10 +0900

    feat: board 레포지토리 작성

---
+++ src/board/board.repository.ts
@@ -0,0 +1,42 @@
+import { dataSource } from "@/core/util/dataSource";
+import { Board } from "@/board/board.entity";
+
+const BoardRepository = dataSource.getRepository(Board).extend({
+  async findByUserId(userId: number) {
+    return this.find({
+      where: { author: { id: userId } },
+    });
+  },
+
+  async findById(id: number) {
+    return this.findOne({
+      where: { id },
+      relations: ["author", "comments"],
+    });
+  },
+
+  async findAll(page: number, limit: number) {
+    return this.findAndCount({
+      skip: (page - 1) * limit,
+      take: limit,
+      order: { createdAt: "DESC" },
+      relations: ["author"],
+    });
+  },
+
+  async createBoard(boardData: Partial<Board>) {
+    const board = this.create(boardData);
+    return this.save(board) as Promise<Board>;
+  },
+
+  async updateBoard(id: number, boardData: Partial<Board>) {
+    await this.update(id, boardData);
+    return this.findById(id) as Promise<Board>;
+  },
+
+  async deleteBoard(id: number) {
+    return this.delete(id);
+  },
+});
+
+export { BoardRepository };
