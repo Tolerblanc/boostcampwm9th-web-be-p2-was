42579d1a102cc898f532ffb5885c7263445016bb
Hyunjun KIM <hihj070914@icloud.com>
2024-10-14 22:13:44 +0900

    feat: board controller 등록

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { WasApplication } from "./core/app";
 import { UserController } from "@/user/user.controller";
 import { IndexController } from "@/index.controller";
 import { handleStaticFileRoute } from "./core/builtin/staticFile.middleware";
+import { BoardController } from "./board/board.controller";
 
 function bootstrap() {
   const app = new WasApplication();
@@ -10,6 +11,7 @@ function bootstrap() {
   app.use(handleStaticFileRoute);
   app.registerControllers(IndexController);
   app.registerControllers(UserController);
+  app.registerControllers(BoardController);
 
   app.listen(3000);
 }
