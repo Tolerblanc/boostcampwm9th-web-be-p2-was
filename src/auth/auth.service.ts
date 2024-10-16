06f928bc1ed12ce31c2aeeaf5974138ab30a08e6
Hyunjun KIM <hihj070914@icloud.com>
2024-10-17 01:59:44 +0900

    feat: Github 로그인 OAuth 로직 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,15 @@
-import { BadRequestError, UnauthorizedError } from "@/core/http/httpError";
+import {
+  BadRequestError,
+  InternalServerError,
+  UnauthorizedError,
+} from "@/core/http/httpError";
 import type { UserRepository } from "@/user/user.repository";
 import SessionStore from "@/core/util/sessionStore";
+import dotenv from "dotenv";
+import { randomUUID } from "crypto";
+import type { User } from "@/user/user.entity";
+
+dotenv.config();
 
 class AuthService {
   constructor(
@@ -24,6 +33,73 @@ class AuthService {
       throw new UnauthorizedError("Invalid email or password");
     const sessionId = this.sessionStore.create(user.id, user.nickname);
     return sessionId;
+  }
+
+  async githubOAuthLogin(code: string) {
+    const accessToken = await this.getGitHubAccessToken(code);
+    const { email, nickname } = await this.getGitHubUser(accessToken);
+
+    let user: User;
+    try {
+      user = await this.userRepository.findByEmail(email);
+    } catch (error) {
+      if (error instanceof InternalServerError) {
+        user = await this.userRepository.createUser({
+          email: email,
+          password: randomUUID(),
+          nickname: nickname,
+        });
+      }
+    }
+    const sessionId = this.sessionStore.create(user!.id, user!.nickname);
+    return sessionId;
+  }
+
+  private async getGitHubAccessToken(code: string): Promise<string> {
+    const response = await fetch(
+      "https://github.com/login/oauth/access_token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        }),
+      }
+    );
+
+    const data = await response.json();
+    return data.access_token;
+  }
+
+  private async getGitHubUser(
+    accessToken: string
+  ): Promise<{ email: string; nickname: string }> {
+    const userResponse = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const userResponseData = await userResponse.json();
+
+    const emailResponse = await fetch("https://api.github.com/user/emails", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const emailResponseData = await emailResponse.json();
+
+    const primaryEmail = emailResponseData.find(
+      (email: { primary: boolean; email: string }) => email.primary
+    ).email;
+
+    return { email: primaryEmail, nickname: userResponseData.login };
   }
 }
 
