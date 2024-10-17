87d8838ce60eb4f3d9df28960690dbc86b11936e
Hyunjun KIM <hihj070914@icloud.com>
2024-10-17 14:38:01 +0900

    refactor: 쿼리 로깅 옵션을 dotenv로 교체

--- src/core/util/dataSource.ts
+++ src/core/util/dataSource.ts
@@ -4,12 +4,16 @@ import { DataSource } from "typeorm";
 import { Board } from "@/board/board.entity";
 import { User } from "@/user/user.entity";
 import { Comment } from "@/comment/comment.entity";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const dataSource = new DataSource({
   type: "sqlite",
-  database: "db.sqlite",
+  database: process.env.DB_NAME as string,
   entities: [User, Board, Comment],
   synchronize: true,
+  logging: Boolean(process.env.QUERY_LOGGING),
 });
 
 export { dataSource };
